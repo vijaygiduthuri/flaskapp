3745927e1831b2779948c7f8d98d4ebc3b6380b9
Vijay Giduthuri <125960600+vijaygiduthuri@users.noreply.github.com>
2025-05-21 21:53:44 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "Hello from Flask APP Hosted on GCP"
+    return "Hello from Flask APP Hosted on GOOGLE CLOUD!"
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
