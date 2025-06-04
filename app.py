6a89c852b3712f6191c5958b239f0813ba62c937
Vijay Giduthuri <125960600+vijaygiduthuri@users.noreply.github.com>
2025-06-04 23:16:34 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "HI ANNAYA!"
+    return "Hello Everyone from Google Cloud Run-0.1!"
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
