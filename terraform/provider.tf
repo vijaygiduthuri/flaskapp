f8973d7b4636041caf9586281f6b99a6868b2098
Vijay Giduthuri <125960600+vijaygiduthuri@users.noreply.github.com>
2025-06-04 13:54:01 +0530

    Update provider.tf

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -1,14 +1,6 @@
-terraform {
-  required_providers {
-    google = {
-      source  = "hashicorp/google"
-      version = "6.21.0"
-    }
-  }
-}
-
 provider "google" {
   project     = var.project_id
   region      = var.region
 #  credentials = "./keys.json"
 }
+
